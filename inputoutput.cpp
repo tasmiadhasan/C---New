ac7dc5999d6106abd4b5cdcec1dcaa5a75831fe9
tasmiadhasan <tasmiad.hasan@northsouth.edu>
2024-06-26 14:30:18 +0600

    new

---
+++ inputoutput.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<conio.h>   
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter a value:";
+    cin>>a;
+    cout<<"The value you entered is:"<<a;
+    getch();
+}   
---
+++ virable.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <conio.h>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    a=20;
+    b=52;
+    cout<<a<<endl<<b;
+    cout<<"\nThe first number is:"<<a<<endl<<"The seond number is:"<<b;    getch();
+
+}
